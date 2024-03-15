d88a44b280358c4d6ed12ab5d8c0f5eb9b15c892
Anssi Piirainen <api@iki.fi>
2024-03-15 09:16:55 +0200

    Add initial flake.nix

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Anssi's Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+        ];
+		
+      system.defaults = {
+         dock.mru-spaces = false;
+         finder = {
+            AppleShowAllExtensions = true;
+            FXPreferredViewStyle = "clmv";
+         };
+         loginwindow = {
+          LoginwindowText = "Norhern Peaks Mac Mini";
+          GuestEnabled = false;
+          SHOWFULLNAME = true;
+         };
+         screencapture.location = "~/Pictures/screenshots";
+         screensaver.askForPasswordDelay = 10;
+      };
+
+      networking.hostName = "anssis-macmini";
+
+      # Allow nonfree packages
+      nixpkgs.config.allowUnfree = true;
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations.anssis-macmini = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations.anssis-macmini.pkgs;
+  };
+}
