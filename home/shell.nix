6068c537912b457556c6895cf7bba757f6cd8491
Anssi Piirainen <api@iki.fi>
2024-06-05 09:28:17 +0300

    add PATH entries for shells

--- home/shell.nix
+++ home/shell.nix
@@ -16,6 +16,9 @@
     };
     initExtra = ''
       export NVM_DIR="$HOME/.nvm"
+      export PATH="$PATH:/Applications/jdk-22.0.1.jdk/Contents/Home/bin/"
+      export JAVA_HOME=/Applications/jdk-22.0.1.jdk/Contents/Home/
+
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
       [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
     '';
@@ -24,4 +27,5 @@
   home.shellAliases = {
     ll = "ls -alh";
   };
+
 }
