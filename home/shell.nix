f4da9fcc8eeed18dcf51582ea99889bafbc1f0ab
Anssi Piirainen <api@iki.fi>
2024-12-16 15:12:33 +0200

    Add /usr/local/bin to path

--- home/shell.nix
+++ home/shell.nix
@@ -25,6 +25,8 @@
       export PATH="/opt/homebrew/opt/php@8.2/bin:$PATH"
       export PATH="/opt/homebrew/opt/php@8.2/sbin:$PATH"
 
+      export PATH="/usr/local/bin:$PATH"
+
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
       [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
