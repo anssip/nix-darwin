c517c372ada132687227433d00932a5468fefc41
Anssi Piirainen <api@iki.fi>
2026-04-10 11:14:16 +0300

    Add ~/.local/bin to PATH

--- home/shell.nix
+++ home/shell.nix
@@ -29,6 +29,8 @@
 
       export PATH="$PATH:$HOME/go/bin"
 
+      export PATH="$PATH:$HOME/.local/bin"
+
       [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
       [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
